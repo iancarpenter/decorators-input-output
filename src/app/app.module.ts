944abfa3cd32a2b0feab6a1da656efbe52f24531
iancarpenter <ianhcarpenter@gmail.com>
2020-09-05 15:42:38 +0100

    initial-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InputComponent } from './input/input.component';
+import { OutputComponent } from './output/output.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    OutputComponent
   ],
   imports: [
     BrowserModule
